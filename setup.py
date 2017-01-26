b9b1be28f15b63d402f526e59da3a64b02671943
Daniel Mooney <ddm7018@rit.edu>
2017-01-26 11:47:00 -0500

    packaging up

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'elo-sports',
+  packages = ['mypackage'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A random test lib',
+  author = 'Daniel Mooney',
+  author_email = 'dantheran7@gmail.com',
+  url = 'https://github.com/ddm7018/Elo', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
