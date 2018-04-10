eede9e0c826bd75a2cec284aba685beed82fc286
Daniel Mooney <ddm7018@rit.edu>
2018-04-09 22:03:37 -0400

    updating package to import correcty, by adding __init__.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-  name = 'elo-sports',
-  packages = ['elo-sports'], # this must be the same as the name above
-  version = '0.1',
+  name = 'elosports',
+  packages = ['elosports'], # this must be the same as the name above
+  version = '0.1.1',
   description = 'Elo Ranking System used for Sports',
   author = 'Daniel Mooney',
   author_email = 'dantheran7@gmail.com',
   url = 'https://github.com/ddm7018/Elo', # use the URL to the github repo
-  download_url = 'https://github.com/ddm7018/Elo/blob/master/dist/elo-sports-0.1.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/ddm7018/Elo/blob/master/dist/elosports-0.1.tar.gz', # I'll explain this in a second
   keywords = ['testing', 'logging', 'example'], # arbitrary keywords
   classifiers = [],
 )
