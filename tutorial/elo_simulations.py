36ced9c0b2de3db1c9127f1905f9af05f4351b1b
Daniel Mooney <ddm7018@rit.edu>
2018-09-02 14:54:40 -0400

    updating tutorials

--- tutorial/elo_simulations.py
+++ tutorial/elo_simulations.py
@@ -1,24 +1,25 @@
 import pandas as pd
-from elosports.elo import Elo
 import datetime 
-
-df = pd.read_csv("nfl_elo.csv")
-
-
-from2000 = df[ (df['date']>'200-06-01')] 
-
-allTeams = set(from2000.team1.tolist())
+from elosports.elo import Elo
 
 
-eloLeague = Elo(k = 20)
+df 			= pd.read_csv("nfl_elo.csv")
+from2000 	= df[ (df['season']>1999)] 
+allTeams 	= set(from2000.team1.tolist())
+eloLeague 	= Elo(k = 20)
 
 for team in allTeams:
 	eloLeague.addPlayer(team)
 
-
+currSeason = 2000
 for game in from2000.iterrows():
+	if game[1].season > currSeason:
+		for key in eloLeague.ratingDict.keys():
+			eloLeague.ratingDict[key] = eloLeague.ratingDict[key] - ((eloLeague.ratingDict[key] - 1500) * (1/3.))
+		currSeason += 1
+
 	if game[1].score1 > game[1].score2:
-		eloLeague.gameOver(game[1].team1, game[1].team2,0)
+		eloLeague.gameOver(game[1].team1, game[1].team2,True)
 	else:
 		eloLeague.gameOver(game[1].team2, game[1].team1,0)
 
